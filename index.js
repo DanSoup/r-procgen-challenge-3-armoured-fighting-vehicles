012c78aad9fe4d113f9842d89dc5cb02e3d22968
Daniel Thomas <Daniel.Thomas@Weaveability.com>
2019-07-19 14:39:15 +0100

    added wheel generation for one side

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const veh = {};
   veh.height = 100 + Math.floor(Math.random() * 51);
   veh.length = 200 + Math.floor(Math.random() * 101);
   veh.width = 100 + Math.floor(Math.random() * 51);
+  veh.wheels = [
+    {
+      size: 10 + Math.floor(Math.random() * 21),
+      position: [5, veh.height - 5]
+    },
+    {
+      size: 10 + Math.floor(Math.random() * 21),
+      position: [veh.length - 5, veh.height - 5]
+    }
+  ]
 }
 
 { // Draw the body.
@@ -27,4 +37,11 @@ const veh = {};
     ctx.fillRect(399 - Math.floor(veh.length / 2) - i, 299 - Math.floor(veh.height / 2) - Math.ceil(i / 2), veh.length, 1);
   }
 
+}
+
+{ // Draw wheels.
+  veh.wheels.forEach(wheel => {
+    ctx.fillStyle = '#666666FF';
+    ctx.fillRect(wheel.position[0] - wheel.size + 400 - Math.floor(veh.length / 2), wheel.position[1] - wheel.size + 300 - Math.floor(veh.height / 2), wheel.size * 2, wheel.size * 2);
+  })
 }
